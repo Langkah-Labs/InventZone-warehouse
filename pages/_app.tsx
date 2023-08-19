935e65b3a753e2784b250b398e4817ce0a990b50
seagalputra <seagalputra@gmail.com>
2023-08-19 16:45:35 +0700

    add dashboard layout and skeleton pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import type { NextPage } from "next";
 import SuperTokenReact, { SuperTokensWrapper } from "supertokens-auth-react";
 
 import { frontendConfig } from "@/config/frontendConfig";
+import { ReactElement, ReactNode } from "react";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 if (typeof window !== "undefined") {
   SuperTokenReact.init(frontendConfig());
 }
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <SuperTokensWrapper>
       <Component {...pageProps} />
     </SuperTokensWrapper>
--- pages/serialnumbers.tsx
+++ pages/serialnumbers.tsx
@@ -6,17 +6,18 @@ import type { NextPageWithLayout } from "./_app";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
-const Home: NextPageWithLayout = () => {
+const SerialNumber: NextPageWithLayout = () => {
   return (
     <main className={`${raleway.className}`}>
-      <h1>Dashboard</h1>
+      {/* TODO: change the content here */}
+      <h1>Serial Number</h1>
     </main>
   );
 };
 
-Home.getLayout = function getLayout(page: ReactElement) {
+SerialNumber.getLayout = function getLayout(page: ReactElement) {
   // TODO: add session auth component
   return <SidebarLayout>{page}</SidebarLayout>;
 };
 
-export default Home;
+export default SerialNumber;
