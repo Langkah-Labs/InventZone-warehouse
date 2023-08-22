4d380ead4f8aec4ef48d64c929c68419af31c442
rizkyramadika <rizky@liquidx.studio>
2023-08-22 23:22:20 +0700

    update required field

--- pages/product-serials/form/index.tsx
+++ pages/product-serials/form/index.tsx
@@ -24,7 +24,7 @@ const findAllProductsQuery = `
 `;
 
 const insertProductSerialMutation = `
-  mutation InsertProductSerialsMutation($serial_number: String!, $product_id: bigint!, $capacity: bigint!, $optical_power: String!, $description: String!) {
+  mutation InsertProductSerialsMutation($serial_number: String!, $product_id: bigint!, $capacity: bigint, $optical_power: String, $description: String) {
     insert_product_serials(objects: {serial_number: $serial_number, product_id: $product_id, capacity: $capacity, optical_power: $optical_power, description: $description}) {
       affected_rows
       returning {
@@ -152,7 +152,7 @@ const SerialProducts: NextPageWithLayout<PageProps> = ({ products }) => {
                   <input
                     type="text"
                     id="capacity"
-                    defaultValue={""}
+                    defaultValue={0}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                     {...register("capacity")}
                   />
