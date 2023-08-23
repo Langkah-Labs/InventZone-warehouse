98f9765e7d5efdc8d6b2b262ef312e0e9442d0d3
rizkyramadika <rizky@liquidx.studio>
2023-08-23 21:33:32 +0700

    update loading, tooltip, alert

--- pages/serial-numbers/form/[id].tsx
+++ pages/serial-numbers/form/[id].tsx
@@ -1,13 +1,15 @@
-import Link from "next/link";
-import SidebarLayout from "@/components/elements/SideBarLayout";
-import { Raleway } from "next/font/google";
-import { ReactElement } from "react";
-import type { NextPageWithLayout } from "../../_app";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { useRouter } from "next/router";
-import { SerialNumber, SerialNumberInput } from "@/types/serial-number";
-import { graphqlRequest } from "@/utils/graphql";
+import { ReactElement, useState, useEffect } from "react";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Raleway } from "next/font/google";
+import { useForm, SubmitHandler } from "react-hook-form";
+import swal from "sweetalert";
+import type { NextPageWithLayout } from "../../_app";
+import { graphqlRequest } from "@/utils/graphql";
+import SidebarLayout from "@/components/elements/SideBarLayout";
+import Loading from "@/components/elements/Loading";
+import { SerialNumber, SerialNumberInput } from "@/types/serial-number";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -75,6 +77,7 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async (
 
 const SerialNumbers: NextPageWithLayout<PageProps> = ({ serialNumber }) => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const {
     register,
     handleSubmit,
@@ -87,12 +90,21 @@ const SerialNumbers: NextPageWithLayout<PageProps> = ({ serialNumber }) => {
     const { id } = router.query;
 
     try {
+      setIsLoading(true);
       await graphqlRequest.request(updateSerialNumberByIdMutation, {
         ...data,
         id,
       });
 
-      router.push("/serial-numbers");
+      swal({
+        title: "Success!",
+        text: "Your data has been saved!",
+        icon: "success",
+      }).then(() => {
+        if (router.isReady) {
+          router.push("/serial-numbers");
+        }
+      });
     } catch (err) {
       console.error(err);
     }
@@ -100,97 +112,101 @@ const SerialNumbers: NextPageWithLayout<PageProps> = ({ serialNumber }) => {
 
   return (
     <main className={`${raleway.className}`}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-12 sm:space-y-16">
-          <div>
-            <h2 className="body-4large-bold leading-7 text-[#113A5D]">
-              Serial Number
-            </h2>
-            <p className="mt-2 max-w-2xl leading-6 body-base-regular text-gray-400">
-              This information will be added as a new serial number data in the
-              system.
-            </p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-12 sm:space-y-16">
+            <div>
+              <h2 className="body-4large-bold leading-7 text-[#113A5D]">
+                Serial Number
+              </h2>
+              <p className="mt-2 max-w-2xl leading-6 body-base-regular text-gray-400">
+                This information will be added as a new serial number data in
+                the system.
+              </p>
 
-            <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="productOrderId"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  No. PO
-                  <span className="text-[#C23A3A]">*</span>
-                </label>
-                <div className="mt-2 sm:col-span-2 sm:mt-0">
-                  <input
-                    type="text"
-                    id="productOrderId"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    defaultValue={serialNumber?.productOrderId ?? ""}
-                    {...register("productOrderId")}
-                  />
+              <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                  <label
+                    htmlFor="productOrderId"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    No. PO
+                    <span className="text-[#C23A3A]">*</span>
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0">
+                    <input
+                      type="text"
+                      id="productOrderId"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      defaultValue={serialNumber?.productOrderId ?? ""}
+                      {...register("productOrderId")}
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Product Name
+                    <span className="text-[#C23A3A]">*</span>
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0">
+                    <input
+                      type="text"
+                      id="name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      defaultValue={serialNumber?.name ?? ""}
+                      {...register("name")}
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                  <label
+                    htmlFor="quantity"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Qty.
+                    <span className="text-[#C23A3A]">*</span>
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0">
+                    <input
+                      type="text"
+                      id="quantity"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      defaultValue={serialNumber?.quantity ?? ""}
+                      {...register("quantity")}
+                    />
+                  </div>
                 </div>
               </div>
-
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  Product Name
-                  <span className="text-[#C23A3A]">*</span>
-                </label>
-                <div className="mt-2 sm:col-span-2 sm:mt-0">
-                  <input
-                    type="text"
-                    id="name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    defaultValue={serialNumber?.name ?? ""}
-                    {...register("name")}
-                  />
-                </div>
-              </div>
-
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="quantity"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  Qty.
-                  <span className="text-[#C23A3A]">*</span>
-                </label>
-                <div className="mt-2 sm:col-span-2 sm:mt-0">
-                  <input
-                    type="text"
-                    id="quantity"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    defaultValue={serialNumber?.quantity ?? ""}
-                    {...register("quantity")}
-                  />
-                </div>
-              </div>
+              <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+                <b>Note:&nbsp;</b>(<span className="text-[#C23A3A]">*</span>) is
+                required
+              </p>
             </div>
-            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
-              <b>Note:&nbsp;</b>(<span className="text-[#C23A3A]">*</span>) is
-              required
-            </p>
           </div>
-        </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <Link
-            href="/serial-numbers"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Cancel
-          </Link>
-          <button
-            type="submit"
-            className="inline-flex justify-center rounded-md bg-[#113A5D] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Save
-          </button>
-        </div>
-      </form>
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <Link
+              href="/serial-numbers"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </Link>
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md bg-[#113A5D] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      )}
     </main>
   );
 };
--- pages/serial-numbers/form/index.tsx
+++ pages/serial-numbers/form/index.tsx
@@ -1,12 +1,14 @@
+import { ReactElement, useState, useEffect } from "react";
 import Link from "next/link";
-import SidebarLayout from "@/components/elements/SideBarLayout";
+import { useRouter } from "next/router";
 import { Raleway } from "next/font/google";
-import { ReactElement } from "react";
 import type { NextPageWithLayout } from "../../_app";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useRouter } from "next/router";
-import { SerialNumberInput } from "@/types/serial-number";
+import swal from "sweetalert";
 import { graphqlRequest } from "@/utils/graphql";
+import { SerialNumberInput } from "@/types/serial-number";
+import SidebarLayout from "@/components/elements/SideBarLayout";
+import Loading from "@/components/elements/Loading";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -25,6 +27,7 @@ const insertSerialNumberMutation = `
 
 const SerialNumbers: NextPageWithLayout = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const {
     register,
     handleSubmit,
@@ -35,9 +38,16 @@ const SerialNumbers: NextPageWithLayout = () => {
     if (!router.isReady) return;
 
     try {
+      setIsLoading(true);
       await graphqlRequest.request(insertSerialNumberMutation, data);
 
-      router.push("/serial-numbers");
+      swal({
+        title: "Success!",
+        text: "Your data has been saved!",
+        icon: "success",
+      }).then(() => {
+        router.push("/serial-numbers");
+      });
     } catch (err) {
       console.error(err);
     }
@@ -45,94 +55,98 @@ const SerialNumbers: NextPageWithLayout = () => {
 
   return (
     <main className={`${raleway.className}`}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-12 sm:space-y-16">
-          <div>
-            <h2 className="body-4large-bold leading-7 text-[#113A5D]">
-              Serial Number
-            </h2>
-            <p className="mt-2 max-w-2xl leading-6 body-base-regular text-gray-400">
-              This information will be added as a new serial number data in the
-              system.
-            </p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-12 sm:space-y-16">
+            <div>
+              <h2 className="body-4large-bold leading-7 text-[#113A5D]">
+                Serial Number
+              </h2>
+              <p className="mt-2 max-w-2xl leading-6 body-base-regular text-gray-400">
+                This information will be added as a new serial number data in
+                the system.
+              </p>
 
-            <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="productOrderId"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  No. PO
-                  <span className="text-[#C23A3A]">*</span>
-                </label>
-                <div className="mt-2 sm:col-span-2 sm:mt-0">
-                  <input
-                    type="text"
-                    id="productOrderId"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    {...register("productOrderId")}
-                  />
+              <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                  <label
+                    htmlFor="productOrderId"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    No. PO
+                    <span className="text-[#C23A3A]">*</span>
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0">
+                    <input
+                      type="text"
+                      id="productOrderId"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      {...register("productOrderId")}
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Product Name
+                    <span className="text-[#C23A3A]">*</span>
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0">
+                    <input
+                      type="text"
+                      id="name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      {...register("name")}
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                  <label
+                    htmlFor="quantity"
+                    className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                  >
+                    Qty.
+                    <span className="text-[#C23A3A]">*</span>
+                  </label>
+                  <div className="mt-2 sm:col-span-2 sm:mt-0">
+                    <input
+                      type="text"
+                      id="quantity"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      {...register("quantity")}
+                    />
+                  </div>
                 </div>
               </div>
-
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  Product Name
-                  <span className="text-[#C23A3A]">*</span>
-                </label>
-                <div className="mt-2 sm:col-span-2 sm:mt-0">
-                  <input
-                    type="text"
-                    id="name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    {...register("name")}
-                  />
-                </div>
-              </div>
-
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="quantity"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  Qty.
-                  <span className="text-[#C23A3A]">*</span>
-                </label>
-                <div className="mt-2 sm:col-span-2 sm:mt-0">
-                  <input
-                    type="text"
-                    id="quantity"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    {...register("quantity")}
-                  />
-                </div>
-              </div>
+              <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+                <b>Note:&nbsp;</b>(<span className="text-[#C23A3A]">*</span>) is
+                required
+              </p>
             </div>
-            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
-              <b>Note:&nbsp;</b>(<span className="text-[#C23A3A]">*</span>) is
-              required
-            </p>
           </div>
-        </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <Link
-            href="/serial-numbers"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Cancel
-          </Link>
-          <button
-            type="submit"
-            className="inline-flex justify-center rounded-md bg-[#113A5D] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Save
-          </button>
-        </div>
-      </form>
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <Link
+              href="/serial-numbers"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </Link>
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md bg-[#113A5D] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      )}
     </main>
   );
 };
