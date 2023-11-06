9129e82de14b5f8b75151f2a3056c8517b872d81
seagalputra <seagalputra@gmail.com>
2023-11-06 12:16:01 +0700

    update cors

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,8 +1,10 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "ODN Apps",
-  apiDomain: "http://localhost:3000",
-  websiteDomain: "http://localhost:3000",
+  apiDomain: "http://191.96.57.242:8085",
+  websiteDomain: "http://191.96.57.242:8085",
+  // apiDomain: "http://localhost:3000",
+  // websiteDomain: "http://localhost:3000",
   apiBasePath: "/api/auth",
   websiteBasePath: "/auth",
 };
